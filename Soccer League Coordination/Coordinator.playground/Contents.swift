87ddbec14afdab846e1f8b4f36953ac2f81b9dd7
Fuad Adetoro <FuadAdetoro@fuads-macbook-pro.local>
2016-07-11 13:24:36 +0100

    Version 2.0 :0... made the required changes!

--- Soccer League Coordination/Coordinator.playground/Contents.swift
+++ Soccer League Coordination/Coordinator.playground/Contents.swift
@@ -1,11 +1,143 @@
-// Soccer Leage Coordinator! 
+/////////////////////////////////////////////
+///////////Soccer Coordinator////////////////
+/////////////////////////////////////////////
 
-// Array for dragon team.
-var dragon: [String] = []
-// Array for raptors team.
-var raptors: [String] = []
-// Array for sharks team.
-var sharks: [String] = []
+
+// Each player has their own dictionary.
+
+var joeSmith: [String: String] = [
+    "name": "Joe Smith",
+    "height": "42",
+    "experience": "Yes",
+    "parent": "Jim and Jan Smith"
+]
+
+var jillTanner: [String: String] = [
+    "name": "Jill Tanner",
+    "height": "36",
+    "experience": "Yes",
+    "parent": "Clara Tanner"
+]
+
+var billBon: [String: String] = [
+    "name": "Bill Bon",
+    "height": "43",
+    "experience": "No",
+    "parent": "Sara and Jenny Bon"
+]
+
+var evaGordon: [String: String] = [
+    "name": "Eva Gordan",
+    "height": "45",
+    "experience": "No",
+    "parent": "Wendy and Mike Gordan"
+]
+
+var mattGill: [String: String] = [
+    "name": "Matt Gill",
+    "height": "40",
+    "experience": "No",
+    "parent": "Charles and Sylvia Gill"
+]
+
+var kimmyStein: [String: String] = [
+    "name": "Kimmy Stein",
+    "height": "41",
+    "experience": "No",
+    "parent": "Bill and Hillary Stein"
+]
+
+var sammyAdams: [String: String] = [
+    "name": "Sammy Adams",
+    "height": "45",
+    "experience": "No",
+    "parent": "Jeff Adams"
+]
+
+var karlSaygan: [String: String] = [
+    "name": "Karl Saygan",
+    "height": "42",
+    "experience": "Yes",
+    "parent": "Heather Bledsoe"
+]
+
+var suzaneGreenberg: [String: String] = [
+    "name": "Suzane Greenberg",
+    "height": "44",
+    "experience": "Yes",
+    "parent": "Henrietta Dumas"
+]
+
+var salDali: [String: String] = [
+    "name": "Sal Dali",
+    "height": "41",
+    "experience": "No",
+    "parent": "Gala Dali"
+]
+
+var joeKavalier: [String: String] = [
+    "name": "Joe Kavalier",
+    "height": "39",
+    "experience": "No",
+    "parent": "Sam and Elaine Kavalier"
+]
+
+var benFinkelstein: [String: String] = [
+    "name": "Ben Finkelstein",
+    "height": "44",
+    "experience": "No",
+    "parent": "Aaron and Kill Finkletstein"
+]
+
+var diegoSoto: [String: String] = [
+    "name": "Diego Soto",
+    "height": "41",
+    "experience": "Yes",
+    "parent": "Robin and Sarika Soto"
+]
+
+var chloeAlaska: [String: String] = [
+    "name": "Chloe Alaska",
+    "height": "47",
+    "experience": "No",
+    "parent": "David and Jamie Alaska"
+]
+
+var arnoldWillis: [String: String] = [
+    "name": "Arnold Willis",
+    "height": "43",
+    "experience": "No",
+    "parent": "Claire Willis"
+]
+
+var phillipHelm: [String: String] = [
+    "name": "Phillip Helm",
+    "height": "44",
+    "experience": "Yes",
+    "parent": "Thomas Helm and Eva Jones"
+]
+
+var lesClay: [String: String] = [
+    "name": "Les Clay",
+    "height": "42",
+    "experience": "Yes",
+    "parent": "Wynonna Brown"
+]
+
+var herschelKrustofski: [String: String] = [
+    "name": "Herschel Krustofski",
+    "height": "45",
+    "experience": "Yes",
+    "parent": "Hyman and Rachel Krustofski"
+]
+
+// Array of players using their dicts
+var players: [[String: String]] = [joeSmith, jillTanner, billBon, evaGordon, mattGill, kimmyStein, sammyAdams, karlSaygan, suzaneGreenberg, salDali, joeKavalier, benFinkelstein, diegoSoto, chloeAlaska, arnoldWillis, phillipHelm, lesClay, herschelKrustofski]
+
+// Dict for teams
+var dragon: [[String: String]] = []
+var raptors: [[String: String]] = []
+var sharks: [[String: String]] = []
 
 // Constants for each teams start date.
 let dragonStartDate = "March 17th, 1pm"
@@ -13,120 +145,48 @@ let raptorsStartDate = " March 17th, 3pm"
 let sharksStartDate = "March 18th, 1pm"
 
 
-// Array of players included in coordination.
-let players: [String] = [
-    "Joe Smith",
-    "Jill Tanner",
-    "Bill Bon",
-    "Eva Gordon",
-    "Matt Gill",
-    "Kimmy Stein",
-    "Sammy Adams",
-    "Karl Saygan",
-    "Suzane Greenberg",
-    "Sal Dall",
-    "Joe Kavaller",
-    "Ben Finkelstein",
-    "Diego Soto",
-    "Chloe Alaska",
-    "Arnold Willis",
-    "Phillip Helm",
-    "Les Clay",
-    "Herschel Krustofkl"
-]
-
-
-// Assign Parent, Height & Experience to player.
-func assignData(player: String) -> (parent: String, height: Int, experience: String) {
-    switch player {
-        case "Joe Smith": return ("Jim and Jan Smith", 42, "Yes")
-        case "Jill Tanner": return ("Clara Tanner", 36, "Yes")
-        case "Bill Bon": return ("Sara and Jenny Bon", 43, "Yes")
-        case "Eva Gordon": return ("Wendy and Mike Gordon", 45, "No")
-        case "Matt Gill": return ("Charles and Sylvia Gill", 40, "No")
-        case "Kimmy Stein": return ("Bill and Hilary Stein", 41, "No")
-        case "Sammy Adams": return ("Jeff Adams", 45, "No")
-        case "Karl Saygan": return ("Heather Bledsoe", 42, "Yes")
-        case "Suzane Greenberg": return ("Henrietta Dumas", 44, "Yes")
-        case "Sal Dall": return ("Gala Dall", 41, "No")
-        case "Joe Kavaller": return ("Sam and Elaine Kavaller", 39, "No")
-        case "Ben Finkelstein": return ("Aaron and Jill Finkelstein", 44, "No")
-        case "Diego Soto": return ("Robin and Sarika Soto", 41, "No")
-        case "Chloe Alaska": return ("David and Jamie Alaska", 47, "Yes")
-        case "Arnold Willis": return ("Claire Willis", 43, "No")
-        case "Phillip Helm": return ("Thomas Helm and Eva Jones", 44, "No")
-        case "Les Clay": return ("Wynonna Brown", 42, "Yes")
-        case "Herschel Krustofkl": return ("Hyman and Rachel Krustofskl", 45, "Yes")
-        default: return ("\(player) isn't in a team", 0, "N/A")
-    }
-}
-
-// Adding players to their correct teams.
-func assignPlayerToTeam(player: String, experience: String){
-    if experience == "Yes" {
-        if player
-        
-        /*if dragon.count == 0 {
-            dragon.append(player)
-        } else if raptors.count == 0 {
-            raptors.append(player)
-        } else if sharks.count == 0 {
-            sharks.append(player)
-        }else{
-            if dragon.count == raptors.count && dragon.count == sharks.count {
-                dragon.append(player)
-            } else if raptors.count < dragon.count && raptors.count == sharks.count {
-                raptors.append(player)
-            } else if sharks.count < raptors.count {
-                sharks.append(player)
+// sortTeams adds players to teams.
+func sortTeams(){
+    // For loop to loop through the players array.
+    for experienceInPlayer in players {
+        // If Experienced
+        if experienceInPlayer["experience"] == "Yes" {
+            //print(experienceInPlayer["name"])
+            if dragon.count < raptors.count {
+                dragon.append(experienceInPlayer)
+                // Call sendLetterToGuardian function and pass parent, name, team and start date.
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "Dragon", startDate: dragonStartDate)
+            } else if raptors.count < sharks.count {
+                raptors.append(experienceInPlayer)
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "Raptors", startDate: raptorsStartDate)
+            } else {
+                sharks.append(experienceInPlayer)
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "Sharks", startDate: sharksStartDate)
             }
-        }*/
-    }else{
-        if dragon.count == 0 {
-            dragon.append(player)
-        } else if raptors.count == 0 {
-            raptors.append(player)
-        } else if sharks.count == 0 {
-            sharks.append(player)
-        }else{
-            if dragon.count == raptors.count && dragon.count == sharks.count {
-                dragon.append(player)
-            } else if raptors.count < dragon.count && raptors.count == sharks.count {
-                raptors.append(player)
-            } else if sharks.count < raptors.count {
-                sharks.append(player)
+        }
+        // If not experienced
+        if experienceInPlayer["experience"] == "No" {
+            //print(experienceInPlayer["name"])
+            if dragon.count < raptors.count {
+                dragon.append(experienceInPlayer)
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "dragon", startDate: dragonStartDate)
+            } else if raptors.count < sharks.count {
+                raptors.append(experienceInPlayer)
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "raptors", startDate: raptorsStartDate)
+            } else {
+                sharks.append(experienceInPlayer)
+                sendLetterToGuardian(experienceInPlayer["parent"]!, player: experienceInPlayer["name"]!, team: "sharks", startDate: sharksStartDate)
             }
         }
     }
 }
 
-// Send letter to Guardian/Parent.
-func sendLetterToGuardian(player_parent: String, player: String) -> String {
-    if raptors.contains(player) {
-        return "Hi \(player_parent). \n\n This letter is in regards to \(player) and the team he's been selected to play for. He will play for the Raptors, the first game is on \(raptorsStartDate). \n\n Kind Regards, \nFootball Coordination"
-    } else if sharks.contains(player) {
-        return "Hi \(player_parent). \n\n This letter is in regards to \(player) and the team he's been selected to play for. He will play for the Sharks, the first game is on \(sharksStartDate). \n\n Kind Regards, \nFootball Coordination"
-    } else {
-        return "Hi \(player_parent). \n\n This letter is in regards to \(player) and the team he's been selected to play for. He will play for the Dragons, the first game is on \(dragonStartDate). \n\n Kind Regards, \nFootball Coordination"
-    }
-}
-
-// loop through players array.
-for player in players {
-    // Assign a player to team.
-    let experience = assignData(player).experience
-    assignPlayerToTeam(player, experience: experience)
-    // player_parent finds out the parents of the player.
-    let player_parent = assignData(player).parent
-    // send letter to Guardian/Parent of player.
-    sendLetterToGuardian(player_parent, player: player)
+// Send letter to parent/Parent.
+func sendLetterToGuardian(player_parent: String, player: String, team: String, startDate: String) {
+    // Printing letter and using string interpolation.
+    print("Hi \(player_parent). \n\n This letter is in regards to \(player) and the team he's been selected to play for. He will play for the \(team), the first game is on \(startDate). \n\n Kind Regards, \nFootball Coordination")
 }
 
 
-
-
-
-
-
-
-
+// Calling sortTeams function so the code runs.
+sortTeams()
